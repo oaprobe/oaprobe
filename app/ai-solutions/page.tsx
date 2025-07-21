3a69279462b3cec2526addee0465e935ff43f50b
shiva <98257507+Shivuduu19@users.noreply.github.com>
2025-07-21 16:06:00 +0530

    ai-solutions page

---
+++ app/ai-solutions/page.tsx
@@ -0,0 +1,192 @@
+import Image from "next/image";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <div className="[font-family:'Neue_Haas_Grotesk_Display_Pro']  mxl:h-[calc(5.57143px+3.09524vw)] mxl:max-h-[calc(5.57143px+3.09524vw)] h-[calc(44.4186px+1.45349vw)] w-full "></div>
+      <div className="pt-(--CALC_HEADER_PADDING_TOP) mt-(--CALC_HEADER_MARGIN_TOP)">
+        <section className="pt-(--CALC_HEADER_PADDING_TOP) mt-(--CALC_HEADER_MARGIN_TOP)">
+          <div className="grid grid-cols-[0fr_1fr] grid-rows-[minmax(344px,auto)] md:grid-cols-[0fr_14fr_2fr] md:grid-rows-[minmax(596px,auto)] mxl:grid-cols-[8fr_14fr_2fr] mxl:grid-rows-[minmax(calc(-5.57143px+31.9048vw),auto)] ">
+            <div></div>
+            <div className=" flex min-w-0 flex-col justify-between [border-left-style:solid] md:pt-[calc(19.4211px+0.986842vw)] pt-[calc(21px+0.78125vw)] mxl:pt-[calc(-5.57143px+2.7381vw)] mxl:pb-[calc(-5.57143px+2.7381vw)] mxl:border-l mxl:gap-[calc(-12.1429px+6.30952vw)] pb-[calc(98px+4.16667vw)] md:pb-[calc(16.8421px+1.97368vw)] border-(--my-colors-border) gap-[49px] md:gap-[59px]">
+              <div className=" grid min-w-0 grid-cols-[1fr] gap-0">
+                <div className=" min-w-0"></div>
+                <div className=" flex min-w-0 flex-col  mxl:px-[calc(-5.71429px+2.38095vw)] px-[calc(8px+1.04167vw)] md:px-[calc(5.89474px+1.31579vw)] mxl:calc(1.14286px+1.19048vw) md:gap-[calc(8.47368px+0.328947vw)] gap-[calc(9px+0.260417vw)]">
+                  <span className=" min-w-0 hidden !leading-[1.3] !font-medium md:text-[calc(16.9474px+0.657895vw)] mxl:text-[calc(-2px+1.66667vw)] text-[calc(18px+0.520833vw)]"></span>
+                  <h1 className=" min-w-0 [font-family:'Neue_Haas_Grotesk_Display_Pro']  !leading-[1.15] !font-medium text-[calc(18px+5.20833vw)] md:text-[calc(20.1053px+4.93421vw)] mxl:text-[calc(-7px+4.16667vw)]">
+                    Enterprise AI solutions development
+                  </h1>
+                </div>
+              </div>
+              <div className=" grid min-w-0 px-[calc(8px+1.04167vw)] md:grid-cols-[6fr_8fr] mxl:grid-cols-[4fr_10fr] mxl:gap-y-0 gap-[calc(40px+1.82292vw)_0px] md:px-0 grid-cols-[4fr_11fr]  md:gap-y-[calc(-47.3684px+10.8553vw)]">
+                <div className=" min-w-0"></div>
+                <div className=" min-w-0 md:px-[calc(5.89474px+1.31579vw)] mxl:px-[calc(-5.71429px+2.38095vw)] "></div>
+              </div>
+            </div>
+          </div>
+
+          {/* secnd section  */}
+          <div className="grid mb-[-1px] [font-family:'Neue_Haas_Grotesk_Display_Pro']  grid-cols-[auto] md:grid-cols-[14fr_2fr] md:mb-0 mxl:grid-cols-[8fr_0fr_16fr]">
+            <a href="" className="z-[1] relative">
+              <div className="m-0 box-border antialiased flex min-w-0 h-full justify-between [transition-property:all] bg-(--my-colors-primary) hover:bg-(--my-colors-secondary) mxl:p-[calc(-5.57143px+2.7381vw)_calc(-5.71429px+2.38095vw)] md:p-[calc(19.4211px+0.986842vw)_calc(5.89474px+1.31579vw)] p-[calc(21px+0.78125vw)_calc(8px+1.04167vw)] duration-200 ease-[ease] delay-[0ms]">
+                <span className=" min-w-0 [transition-property:all] duration-200 ease-[ease] delay-[0ms] text-(--my-colors-primary_text) !leading-[1.4] !font-medium !text-[calc(16.9474px !+ !0.657895vw)]">
+                  Get in touch
+                </span>
+                <div className="p-0 box-border antialiased flex min-w-0 items-center justify-center relative mxl:w-[calc(1.14286px+1.19048vw)] mxl:h-[calc(1.14286px+1.19048vw)] mxl:ml-[calc(1.14286px+1.19048vw)] mxl:mt-[calc(-2.42857px+0.595238vw)]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className=" fill-(--my-colors-primary_text) [transition-property:all] duration-200 ease-[ease] delay-[0ms] stroke-[0] h-full"
+                  >
+                    <path
+                      fill="#000"
+                      d="M17 4.632V3.35H0V0h24v24h-3.349V7h-1.283L2.97 23.398.602 21.03z"
+                      className=" fill-(--my-colors-primary_text) [transition-property:all] duration-200 ease-[ease] delay-[0ms] stroke-[0]"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </a>
+
+            {/* right box  */}
+            <div className="mxl:col-start-3 mxl:col-end-4 md:col-start-1 md:col-end-3 relative">
+              <div className="flex flex-col absolute inset-0 z-[2] h-full w-full">
+                <div className="md:grid-cols-[14fr_2fr] grid flex-[1_1_0%] grid-cols-[1fr_0fr] ">
+                  <div></div>
+                  <div className="bg-(--my-colors-background)"></div>
+                </div>
+                <div className="md:grid-cols-[14fr_2fr] grid grid-cols-[1fr_0fr] ">
+                  <div className="flex justify-end items-end">
+                    <div className="mxl:border-b-[calc(-1.14286px+20.4762vw)] mxl:border-l-[calc(0px+16.6667vw)] mxl:w-[calc(0px+25vw)] border-b-[calc(-18.0233px+32.5581vw)] border-l-[calc(0px+25vw)] w-[calc(0px+37.5vw)] border-l-transparent border-b-white"></div>
+                    <div></div>
+                  </div>
+                </div>
+              </div>
+
+              {/* bg  */}
+              <div className="h-[calc(0px+72.3958vw)] mxl:h-[calc(0px+31.5104vw)] md:h-[calc(5.68434px-14px+50.9328vw)]  overflow-hidden relative w-full ">
+                <div
+                  className="lg:aspect-[auto_1.76309/1] md:aspect-[auto_1.63614/1] flex h-fit relative overflow-hidden bottom-0 aspect-[auto_1.15108/1] *:
+                "
+                >
+                  <Image
+                    src="/Enterprise_AI_Solutions_hero.webp"
+                    alt=""
+                    width={2843.856749311295}
+                    height={1613}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div>
+        <section>
+          <div className="mxl:grid-cols-[2fr_22fr] [font-family:'Neue_Haas_Grotesk_Display_Pro']  grid grid-cols-[1fr]">
+            <div></div>
+            <div className="border-t border-(--my-colors-border) ">
+              <div className="mxl:mt-[calc(-17px+8.33333vw)] mxl:mb-[calc(-17px+8.33333vw)] md:mt-[calc(78.7368px+3.28947vw)] md:mb-[calc(78.7368px+3.28947vw)] m-[calc(80px+3.125vw)_0px]">
+                <div className=" grid min-w-0 grid-cols-[0px_1fr] gap-0">
+                  <div className=" min-w-0" />
+                  <div className=" mxl:ml-[calc(-5.71429px+2.38095vw)] mxl:mr-[calc(-5.71429px+2.38095vw)] mxl:mb-[calc(-10.8571px+5.35714vw)] md:ml-[calc(5.89474px+1.31579vw)] md:mr-[calc(5.89474px+1.31579vw)] md:mb-[calc(51.8421px+1.97368vw)] m-[0px_calc(8px+1.04167vw)_calc(51px+2.08333vw)]">
+                    <div className=" min-w-0">
+                      <h2 className=" min-w-0 !leading-[1.3] !font-medium mxl:text-[calc(-2px+1.66667vw)] text-(--my-colors-secondary) text-[calc(18px+0.520833vw)] md:text-[calc(16.9474px+0.657895vw)]">
+                        What is enterprise AI?
+                      </h2>
+                    </div>
+                    <div className=" min-w-0 mxl:pt-[calc(-2px+1.66667vw)] pt-[calc(18px+0.520833vw)] md:pt-[calc(16.9474px+0.657895vw)]">
+                      <span className=" min-w-0 [transition-property:all] duration-200 ease-[ease] delay-[0ms] !leading-[1.3] !font-medium mxl:text-[calc(-5.57143px+2.7381vw)] md:text-[calc(19.4211px+0.986842vw)] text-(--my-colors-text) text-[calc(21px+0.78125vw)]">
+                        <p className=" [transition-property:all] duration-200 ease-[ease] delay-[0ms] !leading-[1.3] !font-medium pb-0 mxl:text-[calc(-5.57143px+2.7381vw)] md:text-[calc(19.4211px+0.986842vw)]">
+                          Enterprise AI brings artificial intelligence
+                          technologies to large organizations to streamline
+                          operations, improve decision-making, and enhance
+                          performance. It taps into machine learning, natural
+                          language processing (NLP), computer vision, and
+                          predictive analytics.
+                        </p>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+
+                {/* second box  */}
+                <div className="mxl:grid-cols-[10fr_12fr] md:grid-cols-[1fr_1fr]  grid grid-cols-[0px_1fr]">
+                  <div></div>
+                  <div className="grid ">
+                    <div className=" grid min-w-0 gap-0 grid-flow-row mxl:col-auto grid-cols-[auto] col-auto mxl:grid-cols-[repeat(1,_1fr)] md:col-[2_/_3]">
+                      <div className="p-0 mt-0 mxl:mx-[calc(-5.71429px+2.38095vw)]  box-border antialiased min-w-0 md:mx-[calc(5.89474px+1.31579vw)] m-[0px_calc(8px+1.04167vw)]">
+                        <span className=" min-w-0 [transition-property:all] text-(--my-colors-text) duration-200 ease-[ease] delay-[0ms] !leading-[1.4] !font-light mxl:text-[calc(12.9474px+0.657895vw)] md:text-[calc(12.9474px+0.657895vw)]">
+                          <p className=" mxl:text-[calc(1.14286px+1.19048vw)] mxl:pb-[calc(2.28571px+0.714286vw)] [transition-property:all] duration-200 ease-[ease] delay-[0ms] !leading-[1.4] !font-light md:text-[calc(12.9474px+0.657895vw)] md:pb-[calc(8.47368px+0.328947vw)] text-(--my-colors-text) text-[calc(14px+0.520833vw)] pb-[calc(9px+0.260417vw)]">
+                            In the current market, businesses bypassing AI are
+                            setting themselves up for failure. That's why
+                            adopting AI enterprise software is essential for
+                            automating time-consuming tasks (such as in-depth
+                            data analysis and customer service), tackling
+                            operational inefficiencies, and keeping pace with
+                            innovation.
+                          </p>
+                          <p className="  mxl:text-[calc(1.14286px+1.19048vw)] mxl:pb-[calc(2.28571px+0.714286vw)] [transition-property:all] duration-200 ease-[ease] delay-[0ms] !leading-[1.4] !font-light md:text-[calc(12.9474px+0.657895vw)] md:pb-[calc(8.47368px+0.328947vw)] text-(--my-colors-text)">
+                            With the right AI tools, you can secure a
+                            competitive advantage, augment your team's skills,
+                            and modernize systems across various business
+                            operations — supply chain management, customer
+                            service, marketing, finance, cybersecurity, you name
+                            it.
+                          </p>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      {/* the state  */}
+
+      <div>
+        <section>
+          <div className="mxl:grid-cols-[2fr_22fr] [font-family:'Neue_Haas_Grotesk_Display_Pro']  grid grid-cols-[1fr] ">
+            <div></div>
+            <div className="grid grid-cols-1 mxl:border-l border-(--my-colors-border) border-t">
+              <div></div>
+              <div className="m-0 box-border antialiased flex mxl:gap-y-[calc(-2px+1.66667vw)] mxl:p-[calc(-27.4286px+13.0952vw)_calc(-5.71429px+2.38095vw)_calc(-7.28571px+3.45238vw)]  min-w-0 flex-col md:p-[calc(97.1579px+4.27632vw)_calc(5.89474px+1.31579vw)_calc(23.8947px+1.31579vw)] gap-y-[calc(18px+0.520833vw)] p-[calc(98px+4.16667vw)_calc(8px+1.04167vw)_calc(26px+1.04167vw)] md:gap-y-[calc(16.9474px+0.657895vw)]">
+                <h2 className=" min-w-0 !leading-[1.15] !font-medium mxl:text-[calc(-7.28571px+3.45238vw)] md:text-[calc(32.8947px+1.31579vw)] text-(--my-colors-text) text-[calc(26px+1.04167vw)]">
+                  The state of the enterprise artificial intelligence market
+                </h2>
+                <span className=" min-w-0 [transition-property:all] duration-200 ease-[ease] delay-[0ms] !leading-[1.4] !font-light mxl:text-[calc(1.14286px+1.19048vw)] text-(--my-colors-text) text-[calc(14px+0.520833vw)] md:text-[calc(12.9474px+0.657895vw)]">
+                  <p className="mxl:text-[calc(1.14286px+1.19048vw)] mxl:pb-[calc(2.28571px+0.714286vw)]  [transition-property:all] duration-200 ease-[ease] delay-[0ms] !leading-[1.4] !font-light md:text-[calc(12.9474px+0.657895vw)] md:pb-[calc(8.47368px+0.328947vw)] text-(--my-colors-text) text-[calc(14px+0.520833vw)] pb-[calc(9px+0.260417vw)]">
+                    The AI landscape of today is marked by cautious optimism in
+                    embracing artificial intelligence enterprise software.
+                    <a
+                      href="https://www.oreilly.com/radar/ai-adoption-in-the-enterprise-2022/"
+                      target="_blank"
+                      className="p-0 m-0 no-underline box-border antialiased [text-decoration-line:underline] [text-decoration-style:initial] cursor-pointer [text-decoration-skip-ink:none] [text-underline-offset:calc(1.42857px_+_0.238095vw)]"
+                    >
+                      O'Reilly's report
+                    </a>{" "}
+                    reveals a mixed picture: Thirty-one percent of businesses
+                    have yet to adopt AI, while 43 percent are considering its
+                    adoption, and 26 percent have already integrated AI
+                    solutions into their operations.
+                  </p>
+                  <p className="mxl:text-[calc(1.14286px+1.19048vw)] mxl:pb-[calc(2.28571px+0.714286vw)]  [transition-property:all] duration-200 ease-[ease] delay-[0ms] !leading-[1.4] !font-light md:text-[calc(12.9474px+0.657895vw)] md:pb-[calc(8.47368px+0.328947vw)] text-(--my-colors-text)">
+                    Among those with AI in production, only 49 percent have
+                    established a governance plan to oversee AI deployment.
+                  </p>
+                </span>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default page;
